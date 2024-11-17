6036971e07c6421237d82057ed73cd871f36bc03
oscar9210 <mr.niceguy9210@gmail.com>
2024-11-17 22:12:34 +0800

    First Commit

--- src/main/java/com/Oscar/latihan2pbo/JFrame.java
+++ src/main/java/com/Oscar/latihan2pbo/JFrame.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package com.Auryn.latihan2pbo;
+package com.Oscar.latihan2pbo;
 import com.toedter.calendar.JDateChooser;
 import java.time.LocalDate;
 import java.time.Period;
--- src/main/java/com/Oscar/latihan2pbo/Latihan2PBO.java
+++ src/main/java/com/Oscar/latihan2pbo/Latihan2PBO.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.Auryn.latihan2pbo;
+package com.Oscar.latihan2pbo;
 
 /**
  *
